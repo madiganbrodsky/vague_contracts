4d3adc3d0f2c70764452d6dcb01288d20e6583bc
Madigan Brodsky <madiganbrodsky@gmail.com>
2023-05-12 12:00:30 -0700

    added another posthoc viz

--- analyses/1_falseconsensus/post_hoc/1_falseconsensus/posthoc_relationship/posthoc_relationship.R
+++ analyses/1_falseconsensus/post_hoc/1_falseconsensus/posthoc_relationship/posthoc_relationship.R
@@ -13,14 +13,14 @@ cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00",
 source("/Users/madiganbrodsky/Desktop/legal_interpretation/vague_contracts/analyses/helpers.R")
 
 # load core dataset
-d <- read_csv("/Users/madiganbrodsky/Desktop/legal_interpretation/vague_contracts/results/1_falseconsensus/main-merged.csv")
+d <- read_csv("/Users/madiganbrodsky/Desktop/legal_interpretation/vague_contracts/data/1_falseconsensus/main-merged.csv")
 
 # load extra information
-metadata = read_csv("/Users/madiganbrodsky/Desktop/legal_interpretation/vague_contracts/experiments/1_falseconsensus/main/stimuli.csv") %>% 
-  select(Title, version, locus_of_uncertainty, locus_of_uncertainty_location,name_gender) %>% 
+metadata = read_csv("/Users/madiganbrodsky/Desktop/legal_interpretation/vague_contracts/experiments/1_falseconsensus/main/stimuli.csv") %>%
+  select(Title, version, locus_of_uncertainty, locus_of_uncertainty_location,name_gender) %>%
   rename(title=Title)
 
-d = d %>% 
+d = d %>%
   left_join(metadata,by=c("title","version"))
 
 # NATIVE LANGUAGE EXCLUSIONS
@@ -39,7 +39,7 @@ excludedWorkers <- (d %>%
                       summarise(n = n()) %>%
                       filter(n > 1))$workerid
 
-d <- d %>% 
+d <- d %>%
   filter(!(workerid %in% excludedWorkers)) %>%
   filter(!(workerid %in% excludedWorkers_nonNative))
 
@@ -52,7 +52,7 @@ transformed <- d %>%
   ungroup() %>%
   group_by(version,title,individual_judgment,locus_of_uncertainty_location,name_gender) %>%
   mutate(count = n()) %>%
-  mutate(true_proportion = binom.confint(x = count, n  = total, methods = "exact")$mean * 100, 
+  mutate(true_proportion = binom.confint(x = count, n  = total, methods = "exact")$mean * 100,
          ci_low = binom.confint(x = count, n  = total, methods = "exact")$lower * 100,
          ci_high = binom.confint(x = count, n  = total, methods = "exact")$upper * 100,
          agreement_mean = mean(population_judgment)) %>%
@@ -65,11 +65,11 @@ levels(transformed$versionPretty) <- c("Controversial", "Covered", "Not\ncovered
 # how many of each type of item
 items = unique(transformed[,c("locus_of_uncertainty_location","title")])
 table(items$locus_of_uncertainty_location)
-#  definition_exh definition_incl       exclusion 
-#         35               3               8 
+#  definition_exh definition_incl       exclusion
+#         35               3               8
 
 new <- transformed %>%
-  filter(version == "controversial") %>% #%>% 
+  filter(version == "controversial") %>% #%>%
   group_by(title) %>% ## 'title' is added for posthoc analysis
   mutate(yes = case_when(individual_judgment == "yes" ~ 1,
                          TRUE ~ 0),
@@ -77,12 +77,12 @@ new <- transformed %>%
                         TRUE ~ 0),
          cantdecide = case_when(individual_judgment == "cantdecide" ~ 1,
                                 TRUE ~ 0)) %>%
-  summarise(nYes = sum(yes), nNo =sum(no), nCantDecide = sum(cantdecide), 
+  summarise(nYes = sum(yes), nNo =sum(no), nCantDecide = sum(cantdecide),
             propYes = sum(yes)/n(), propNo = sum(no)/n(), propCantDecide = sum(cantdecide)/n(), meanError = mean(difference)) %>%
-  as.data.frame() %>%  
-  arrange(propYes) %>% 
-  mutate(title = replace(title, title == 'Goods Carrying Vehicle: Loss or Damage to Vehicle', 'Goods Carrying Vehicle')) %>% 
-  filter(grepl('Escape of Oil II|Vehicle Glass II|Emergency Damages I|Malicious Acts or Vandalism II|Ground Heave I|Storm Damage|Vehicle Fire III|House Removal I|General Damages|Escape of Water I|Trace and Access I', title)) #REMOVE THIS FILTER TO SEE PLOT OF ALL ITEMS!!!
+  as.data.frame() %>%
+  arrange(propYes) %>%
+  mutate(title = replace(title, title == 'Goods Carrying Vehicle: Loss or Damage to Vehicle', 'Goods Carrying Vehicle'))
+  # filter(grepl('Escape of Oil II|Vehicle Glass II|Emergency Damages I|Malicious Acts or Vandalism II|Ground Heave I|Storm Damage|Vehicle Fire III|House Removal I|General Damages|Escape of Water I|Trace and Access I', title)) #REMOVE THIS FILTER TO SEE PLOT OF ALL ITEMS!!!
 
 
 #Turn your 'title' column into a character vector
@@ -122,6 +122,22 @@ ggplot(data = new,
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
         plot.title = element_text(hjust = 0.5)) + 
   ggtitle("Relationship between the proportion of 'yes' responses and estimation error for controversial items ") 
+
+
+#PLOT #3: PROP YES / ERROR FOR CONTROVERSIAL STIMS 
+
+
+new2 <- transformed %>% 
+  filter(version != 'controversial', 
+    individual_judgment != 'cantdecide')
+
+ggplot(data = new2, 
+       mapping = aes(x = individual_judgment, 
+                     y = population_judgment)) + 
+  geom_jitter(aes(color = individual_judgment), 
+              alpha = 0.3)+ 
+  facet_wrap(~confidence) 
+
 
 
   
